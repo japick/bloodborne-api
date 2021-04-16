e7226a1e3d8d839f1f3bacaad821a50fd3e60bbb
japick <jay.pick.me@gmail.com>
2021-04-16 18:26:51 +0100

    schema updates

--- src/models/trickWeaponSchema.js
+++ src/models/trickWeaponSchema.js
@@ -3,54 +3,94 @@ const mongoose = require('mongoose')
 const trickWeaponSchema = mongoose.Schema({
   name: String,
   description: String,
+  image: String,
   physical_atk: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   blood_atk: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   arcane_atk: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   fire_atk: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   bolt_atk: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   slow_poison: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   rapid_poison: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   vs_kin: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   vs_beasts: {
     type: Number,
-    default: null,
-    required: false
+    default: null
   },
   strength: {
     base: Number,
-    bonus: String
+    bonus: Array
+  },
+  skill: {
+    base: Number,
+    bonus: Array
+  },
+  bloodtinge: {
+    type: Number,
+    default: null
+  },
+  arcane: {
+    base: Number,
+    bonus: Array
+  },
+  durability: {
+    type: Number,
+    default: null
+  },
+  qs_bullet_use: {
+    type: Number,
+    default: null
+  },
+  rally: {
+    type: Number,
+    default: null
+  },
+  bg_slots: {
+    type: Object,
+    normal: {
+      type: Array,
+      default: null
+    },
+    normal: {
+      type: Array,
+      default: null
+    },
+    normal: {
+      type: Array,
+      default: null
+    }
+  },
+  location: String,
+  buy_price: {
+    type: Number,
+    default: null
+  },
+  sell_price: {
+    type: Number,
+    default: null
   }
 })
 
--- src/routes/trickWeapons.js
+++ src/routes/trickWeapons.js
@@ -6,7 +6,7 @@ const trickWeapon = require('../models/trickWeaponSchema')
 router.get('/', async (req, res) => {
   // return data
   try {
-    const weapons = await trickWeapon.find()
+    const weapons = await trickWeapon.find({}, '-_id -__v') // exclude fields
     res.json(weapons)
   } catch(error) {
     res.json({ message: error })
@@ -17,11 +17,41 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const weapon = new trickWeapon({
     name: req.body.name,
-    // needs all definitions here...
+    description: req.body.description,
+    image: req.body.image,
+    physical_atk: req.body.physical_atk,
+    blood_atk: req.body.blood_atk,
+    arcane_atk: req.body.arcane_atk,
+    fire_atk: req.body.fire_atk,
+    bolt_atk: req.body.bolt_atk,
+    slow_poison: req.body.slow_poison,
+    rapid_poison: req.body.rapid_poison,
+    vs_kin: req.body.vs_kin,
+    vs_beasts: req.body.vs_beasts,
     strength: {
       base: req.body.strength.base,
       bonus: req.body.strength.bonus
-    }
+    },
+    skill: {
+      base: req.body.skill.base,
+      bonus: req.body.skill.bonus
+    },
+    bloodtinge: req.body.bloodtinge,
+    arcane: {
+    	base: req.body.arcane.base,
+    	bonus: req.body.arcane.bonus
+    },
+    durability: req.body.durability,
+    qs_bullet_use: req.body.qs_bullet_use,
+    rally: req.body.rally,
+    bg_slots: {
+    	normal: req.body.bg_slots.normal,
+    	uncanny: req.body.bg_slots.uncanny,
+    	lost: req.body.bg_slots.lost
+    },
+    location: req.body.location,
+    buy_price: req.body.buy_price,
+    sell_price: req.body.sell_price
   })
   try {
     const savedWeapon = await weapon.save()
