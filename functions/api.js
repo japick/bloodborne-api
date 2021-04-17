e68f0a59e93ea5526dfcb67414937f36df1e8a75
japick <jay.pick.me@gmail.com>
2021-04-17 19:54:56 +0100

    lambda functions

--- functions/api.js
+++ functions/api.js
@@ -3,13 +3,13 @@ const serverless = require('serverless-http')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const trickWeaponsEndpoint = require('./routes/trickWeapons')
+const trickWeaponsEndpoint = require('../routes/trickWeapons')
 
 require('dotenv/config')
 
 app.use(bodyParser.json())
 
-app.use('/.netlify/functions/api/trick-weapons', trickWeaponsEndpoint)
+app.use('/api/trick-weapons', trickWeaponsEndpoint)
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
