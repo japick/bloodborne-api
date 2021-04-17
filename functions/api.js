740c4e1f4e0941cc48e26735266401009b896ba5
japick <jay.pick.me@gmail.com>
2021-04-17 20:11:37 +0100

    netlify-lambda test

--- functions/api.js
+++ functions/api.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const serverless = require('serverless-http')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const trickWeaponsEndpoint = require('../routes/trickWeapons')
+// const express = require('express')
+// const serverless = require('serverless-http')
+// const app = express()
+// const mongoose = require('mongoose')
+// const bodyParser = require('body-parser')
+// const trickWeaponsEndpoint = require('../routes/trickWeapons')
 
-require('dotenv/config')
+// require('dotenv/config')
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
-app.use('/api/trick-weapons', trickWeaponsEndpoint)
+// app.use('/api/trick-weapons', trickWeaponsEndpoint)
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-module.exports.handler = serverless(app)
+// module.exports.handler = serverless(app)
+
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: "It works"
+  })
+}
