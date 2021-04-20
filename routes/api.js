c680947fe78d659b3c7422190b9ab4fdde4d09c5
japick <jay.pick.me@gmail.com>
2021-04-20 19:46:02 +0100

    updated routes

--- routes/api.js
+++ routes/api.js
@@ -13,30 +13,12 @@ router.get('/trick-weapons', (req, res, next) => {
     .catch(next)
 })
 
-// get a list of all consumable items
-router.get('/consumables', (req, res, next) => {
-  Consumable.find({}, '-_id -__v')
-    .then((consumables) => {
-      res.send(consumables)
-    })
-    .catch(next)
-})
-
 // POST...
 // add trick weapon
 router.post('/trick-weapons', (req, res, next) => {
   TrickWeapon.create(req.body)
     .then((weapon) => {
       res.send(weapon)
-    })
-    .catch(next)
-})
-
-// add consumable
-router.post('/consumables', (req, res, next) => {
-  Consumable.create(req.body)
-    .then((consumable) => {
-      res.send(consumable)
     })
     .catch(next)
 })
